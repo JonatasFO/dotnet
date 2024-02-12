eeb9e6bfde8ebc72eeaad8bc89ceb64585459f1f
Jonatas Ferreira <jonatas_098@hotmail.com>
2024-02-11 22:38:02 -0300

    gitignore

--- obj/Debug/net6.0/FilmesAPI.AssemblyInfo.cs
+++ obj/Debug/net6.0/FilmesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+672d5fb3a415fefafa16d81f0c7585952eecfbfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
